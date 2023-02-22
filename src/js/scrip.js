e7cb044f49e25e36cc70d26bb5caaa15ffb9372b
Oleksii <kravchenko.a.a.rb@gmail.com>
2023-02-22 19:50:43 +0200

    add skill raiting

--- src/js/scrip.js
+++ src/js/scrip.js
@@ -10,3 +10,9 @@ closeElem.addEventListener('click', () => {
     menu.classList.remove('active');
 });
 
+const counters = document.querySelectorAll('.skill__raiting-counter'),
+    lines = document.querySelectorAll('.skill__raiting-line span');
+
+counters.forEach( (item, i) => {
+    lines[i].style.width = item.innerHTML;
+});
